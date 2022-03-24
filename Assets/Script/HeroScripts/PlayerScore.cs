6a4017b0b4928b078046a1d9c1cf3686d6bc26a7
Nechalate <p1-18@stud.local>
2022-03-24 10:13:02 +0300

    added ui player score

--- Assets/Script/HeroScripts/PlayerScore.cs
+++ Assets/Script/HeroScripts/PlayerScore.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
     Vector3 startPosition;
     [SerializeField] ParticleSystem crash;
+    [SerializeField] Text _scoreNumber;
+    Text score;
     private int Score = 0;
     void Start() {
         startPosition = transform.position;
+        score = _scoreNumber.GetComponent<Text>();
     }
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Obstacle") {
@@ -16,10 +18,12 @@ public class PlayerScore : MonoBehaviour
             if (!crash.isPlaying) crash.Play();
             else crash.Stop();
             transform.position = startPosition;
+            score.text = Score.ToString();
             Debug.Log($"You Bumped! Total: {Score}");
         }
         if (other.gameObject.tag == "Star") {
             Score += 5;
+            score.text = Score.ToString();
             Debug.Log($"You take star! Total: {Score}");
         }
     }
